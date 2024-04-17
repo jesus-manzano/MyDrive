a6aedff95929aff7a12df6fc658577d7b30c6211
jesus-manzano <manzanoal148@gmail.com>
2024-04-17 02:05:00 +0200

    Updating frontend to integrate Vue Router and Axios

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld :msg="msg"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      msg: '',
+    };
+  },
+  methods: {
+    fetchData() {
+      // Hacer una petición GET al backend
+      axios.get('/api/mensajes/hola')
+          .then(response => {
+            // Al recibir la respuesta, asignar los datos a responseData
+            this.msg = response.data;
+          })
+          .catch(error => {
+            // Manejar errores de la petición
+            console.error('Error al obtener datos:', error);
+          });
+    },
+  },
+  mounted() {
+    // Llamar al método fetchData al montar el componente
+    this.fetchData();
+  }
+}
+</script>
