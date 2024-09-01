caf6c1aa72bb6f5b94ff4baa9d6645384d9df319
jesus-manzano <manzanoal148@gmail.com>
2024-09-01 20:51:36 +0200

    added javadoc documentation for ProviderParam

--- backend/src/main/java/com/mydrive/backend/annotations/ProviderParam.java
+++ backend/src/main/java/com/mydrive/backend/annotations/ProviderParam.java
@@ -1,5 +1,25 @@
 package com.mydrive.backend.annotations;
 
+/**
+ * Anotación personalizada para inyectar el parámetro `provider` en todos los endpoints que lo requieran.
+ *
+ * <p>Esta anotación se utiliza en los controladores para asegurar que el parámetro `provider` se
+ * documente y sea requerido en las operaciones expuestas en Swagger. El `provider` indica el
+ * proveedor de almacenamiento en la nube (por ejemplo, "google-drive" o "dropbox") y se pasa
+ * como un parámetro en la URL.</p>
+ *
+ * <p>El parámetro `provider` es obligatorio y se espera que su valor sea uno de los siguientes:
+ * <ul>
+ *   <li>google-drive</li>
+ *   <li>dropbox</li>
+ * </ul>
+ * </p>
+ *
+ * @see io.swagger.v3.oas.annotations.Operation
+ * @see io.swagger.v3.oas.annotations.Parameter
+ * @see io.swagger.v3.oas.annotations.enums.ParameterIn
+ * @see io.swagger.v3.oas.annotations.media.Schema
+ */
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
